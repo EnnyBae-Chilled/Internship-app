096dc111039fdbda2a748daa0b2ffc5e17cd442d
EnnyBae-Chilled <eniolafarinde@gmail.com>
2025-04-30 16:39:54 -0500

    I removed the service-worker

--- src/index.js
+++ src/index.js
@@ -32,18 +32,3 @@ window.addEventListener("beforeinstallprompt", (e) => {
     });
   }
 });
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/service-worker.js")
-      .then((registration) => {
-        console.log(
-          "Service Worker registered with scope:",
-          registration.scope
-        );
-      })
-      .catch((error) => {
-        console.error("Service Worker registration failed:", error);
-      });
-  });
-}
