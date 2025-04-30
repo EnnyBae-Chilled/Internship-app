30c275009bc4298061967e2ca6729d2436f0efc1
EnnyBae-Chilled <eniolafarinde@gmail.com>
2025-04-30 16:49:10 -0500

    I removed the all serviceworker registration from index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
 import React from "react";
-// import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-// import reportWebVitals from "./reportWebVitals";
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import { createRoot } from "react-dom/client";
+
+// Create the root element for React to render
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(<App />);
 
-// Register service worker
-serviceWorkerRegistration.register();
-
-// Install prompt
+// Install prompt for PWA
 let deferredPrompt;
 window.addEventListener("beforeinstallprompt", (e) => {
   e.preventDefault();
